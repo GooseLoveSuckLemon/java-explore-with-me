1c605b5c9939e17855302a71ce540df33ede0a2d
Goose <lnpscoop@gmail.com>
2026-06-24 00:50:15 +0500

    В классе StatsController pattern вынесен в константу, в классе StatsServiceImpl индексы вынесены в сответствующие константы

--- stats-server/src/main/java/practicum/controller/StatsController.java
+++ stats-server/src/main/java/practicum/controller/StatsController.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
