d45ac0fc06a467d2d8fec65b35b8d56f3965c8f0
Goose <lnpscoop@gmail.com>
2026-07-05 02:35:55 +0500

    jes5

--- main-server/src/main/java/ru/practicum/explore/controller/public_controller/PublicEventController.java
+++ main-server/src/main/java/ru/practicum/explore/controller/public_controller/PublicEventController.java
@@ -1,6 +1,5 @@
 package ru.practicum.explore.controller.public_controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
